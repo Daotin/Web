9e20ac8c17796f7436d6ba9c642c075a898b9656
Daotin <17607171931@163.com>
2019-02-22 10:29:10 +0800

    删除警告文件

--- @小项目/04-仿JD移动端/zepto-master/src/amd_layout.js
+++
@@ -1,9 +0,0 @@
-(function(global, factory) {
-  if (typeof define === 'function' && define.amd)
-    define(function() { return factory(global) })
-  else
-    factory(global)
-}(this, function(window) {
-  YIELD
-  return Zepto
-}))
